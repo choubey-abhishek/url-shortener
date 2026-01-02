51bb5ddc47d3cb3649406dc92b3ff0bbc5d373aa
choubeyji0 <Abhishekchoubey312@gmail.com>
2026-01-02 21:27:55 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+from app.routes.shorten import router as shorten_router
 
+app = FastAPI()
+
+app.include_router(shorten_router)
+
+@app.get("/")
+def home():
+    return {"message": "URL Shortener is running"}
