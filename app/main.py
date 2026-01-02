4aae24dadcd8dbfb4dd0b075aa2f66ab5e42c24e
choubeyji0 <Abhishekchoubey312@gmail.com>
2026-01-02 21:36:07 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,3 +8,8 @@ app.include_router(shorten_router)
 @app.get("/")
 def home():
     return {"message": "URL Shortener is running"}
+
+from app.database import Base, engine
+from app.models import URL
+
+Base.metadata.create_all(bind=engine)
