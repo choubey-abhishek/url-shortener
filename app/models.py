6f52a6bc3172363461b9fb9ca330176f5ebbc801
choubeyji0 <Abhishekchoubey312@gmail.com>
2026-01-03 12:19:37 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,7 +3,6 @@ from app.database import Base
 
 class URL(Base):
     __tablename__ = "urls"
-
     id = Column(Integer, primary_key=True, index=True)
     long_url = Column(String, nullable=False)
-    short_code = Column(String, unique=True, index=True)
+    short_code = Column(String, unique=True, index=True, nullable=False)
